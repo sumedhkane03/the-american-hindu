a73f075998b01f4c3df18bbeed584e662b74884d
atontalapur <91762451+atontalapur@users.noreply.github.com>
2024-09-07 09:21:26 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,26 @@ import React from "react";
 import Header from "../components/Header";
 import Image from "next/image";
 import Member from "../components/Member";
+import "../styles/Book.css";
+import "@fontsource/clear-sans";
+import MobileHeader from "@/components/MobileHeader";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
+    <div className="min-h-screen bg-gray-100 tah-clear-sans bg-gradient-to-br from-red-600 via-navy-900 to-sky-400">
+      {/* <Header /> */}
+      <MobileHeader />
       <main>
+        <section id="book" className="min-h-screen flex flex-row items-center justify-center text-white px-4 py-8 mt-5 custom-book">
+          <h2 className="text-center tah-clear-sans mb-12 ">
+            The Hindu-American Mosaic is the inaugural publication from The American Hindu,
+            offering a tribute to the history and evolution of the Hindu community in the United States.
+            Through recounts of various chapters of this multi-century history,
+            this book delves into the diverse experiences and contributions of Hindu-Americans,
+            celebrating their cultural heritage and impact on the broader American landscape.
+          </h2>
+          <img className="max-w-[55%] max-h-[55%] book-img-blur book-img-style" src="/book-rhs.png" />
+        </section>
         <section
           id="home"
           className="min-h-screen flex flex-col items-center justify-center text-white px-4 py-8 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400"
@@ -23,7 +37,7 @@ export default function Page() {
                 {/* Decreased image size */}
                 <Image
                   src="/swami.jpg"
-                  alt="HMSA Logo"
+                  alt="TAH Logo"
                   fill
                   style={{ objectFit: "contain" }}
                   className="rounded-lg"
@@ -86,28 +100,55 @@ export default function Page() {
             </a>
           </div>
         </section>
-        <section id="about" className="py-8 bg-gray-100">
-          <div className="container mx-auto px-4 max-w-7xl">About section</div>
+        <section id="about" className="min-h-screen flex flex-col items-center justify-center text-white px-4 py-8 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400">
+          <div className="container mx-auto px-4 max-w-7xl">
+            <p className="text-lg sm:text-xl lg:text-2xl text-center mb-8">
+              The American Hindu is a dynamic, youth-driven initiative
+              that began as an Instagram content creator in January.
+            </p>
+            <p className="text-md sm:text-lg lg:text-xl text-center mb-8">
+              Seeing a lack of informative content on Hindu
+              culture, Hindu-American students and young professionals
+              from around the country came together to fill the void on
+              social media. Focusing on education and advocacy of
+              Hindu topics, the page features several post series such as
+              Hinduism 101, Sanskrit Non-Translatables, Festival Posts,
+              and Trending Topics.
+            </p>
+            <p className="text-md sm:text-lg lg:text-xl text-center mb-8">
+              The American Hindu has since evolved into a self-publishing organization specializing in a diverse range of
+              Hindu educational content. Through a blend of traditional
+              and modern mixed-media formats, including print and
+              digital publications, videos, and interactive resources, the
+              organization aims to make Hindu culture, philosophy, and
+              history accessible and engaging for a wide audience. By
+              empowering young creators and fostering a collaborative
+              environment, The American Hindu preserves and promotes
+              cultural heritage and encourages the exploration and
+              understanding of Hindu traditions in a contemporary
+              context.
+            </p>
+          </div>
         </section>
-        <section id="team" className="bg-gradient-to-br from-red-600 via-navy-900 to-sky-400">
+        <section id="team" className="min-h-screen flex flex-col items-center justify-center text-white px-4 py-8 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400">
           <div className="team-page-container">
-          <Member
+            <Member
               picture={
-              <img src={"/mangesh-headshot.jpeg"}   />
-            }
-              name='Sumedh Kane'
+                <img src={"/mangesh-headshot.jpeg"} />
+              }
+              name='Mangesh Ahirrao'
               role='Developer'
               major="Computer Science"
-              ig="https://www.instagram.com/sumedhkane/"
-              li="https://www.linkedin.com/in/sumedh-kane/"
-              git="https://github.com/sumedhkane03"
+              ig="https://www.instagram.com/mangeshjahirrao/"
+              li="https://www.linkedin.com/in/mangeshjahirrao/"
+              git="https://github.com/MangeshJAhirrao"
               id=""
               fb="false"
-          />
-          <Member
+            />
+            <Member
               picture={
                 <img src="/sumedh-headshot.png"></img>
-            }
+              }
               name='Sumedh Kane'
               role='Developer'
               major="Computer Science"
@@ -116,20 +157,50 @@ export default function Page() {
               git="https://github.com/sumedhkane03"
               id="sumedh"
               fb="false"
-          />
-          <Member
+            />
+            <Member
               picture={
-              <img src={"/swami.jpg"} />
-            }
+                <img src={"/swami.jpg"} />
+              }
               name='New Member'
               role='Hindu'
               major="Hindu"
-              ig=""
+              ig="https://www.instagram.com/theamericanhindu/"
               li=""
               git=""
               id="mangesh"
               fb="false"
-          />
+            />
+          </div>
+        </section>
+        <section id="join" className="min-h-screen flex flex-col items-center justify-center text-white px-4 py-8 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400">
+
+          <div className="container mx-auto px-4 max-w-7xl">
+            <div className="flex flex-col md:flex-row items-start justify-between">
+              <div className="text-white md:w-2/3 pr-0 md:pr-8">
+                <h2 className="text-4xl xs:text-5xl lg:text-6xl font-bold mb-4 text-left">
+                  Join Our Community
+                </h2>
+                <p className="text-xl sm:text-2xl mb-6 text-left">
+                  Become a part of the Hindu Medical Society of America and
+                  contribute to our mission of integrating Hindu principles with
+                  modern medicine.
+                </p>
+                <a
+                  href="https://forms.gle/W6zBeMVQzswTN1CK8"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <button className="bg-white text-hmsa-blue font-bold py-2 px-6 rounded-full hover:bg-gray-200 transition duration-300">
+                    Join HMSA
+                  </button>
+                </a>
+              </div>
+              <div className="md:w-1/3 mt-8 md:mt-0 flex items-center justify-center md:justify-end">
+                Hello
+              </div>
+            </div>
           </div>
         </section>
       </main>
