ccf250b03c3835b73dae9e8545257d1df9353e0a
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2024-09-07 13:09:36 -0400

    buyBook-2

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import { BsFacebook } from 'react-icons/bs'
-import "../styles/Team.css";
+import { BsFacebook, BsInstagram, BsTwitter, BsTwitterX, BsYoutube } from 'react-icons/bs'
+import "../styles/Header.css";
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 
 function MobileHeader() {
   return (
     <>
-    <div className='container mx-auto flex justify-center items-center mb-4'>
-        <div className="flex items-center space-x-4">
-                <div className="rounded-full overflow-hidden max-w-[200px] max-h-[200px] bg-red-100">
+    <div className='mx-auto flex justify-center flex-col items-center mb-4 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400'>
                 <img
                     src="swami.jpg"
                     alt="The American Hindu Logo"
-                    className="w-full h-full object-cover"
+                    className="w-50 h-50 object-cover rounded-full overflow-hidden max-w-[100px] max-h-[100px] bg-red-100 mt-2"
                 />
-                </div>
-            </div>
+            
+    
+    <div className="header-socials">
+    <a target="_blank" href="https://google.com"><BsFacebook/></a>
+    <a target="_blank" href="https://google.com"><BsInstagram/></a>
+    <a target="_blank" href="https://google.com"><BsTwitterX/></a>
+    <a target="_blank" href="https://google.com"><BsYoutube/></a>
     </div>
-    <div className="member-socials">
-        <BsFacebook/>
+    
     </div>
   </>
   )
 }
 
-export default MobileHeader
+export default MobileHeader;
