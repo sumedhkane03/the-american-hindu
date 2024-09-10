49bf014f379468b5db073a69020476faf8e4ebb6
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2024-09-10 11:48:55 -0400

    HamburgerMenu-ClickableLinks-sk

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,26 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import { BsFacebook, BsInstagram, BsTwitter, BsTwitterX, BsYoutube } from 'react-icons/bs'
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsTwitterX,
+  BsYoutube,
+} from "react-icons/bs";
 import "../styles/Header.css";
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
-
+import { usePathname } from "next/navigation";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { MdClose, MdMenu } from "react-icons/md";
+import Link from "next/link";
 
 function MobileHeader() {
+  const [menu, setMenu] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  const pathname = usePathname();
+
+  const menuLeave = () => {
+    const coolMenu = document.getElementById("cool-menu");
+    if (coolMenu !== null) {
+      coolMenu.classList.add("menu-leave");
+      coolMenu.classList.remove("menu-enter");
+    }
+
+    setTimeout(() => {
+      setMenu(false);
+    }, 400);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ["home", "about", "join"];
+      const currentSection = sections.find((section) => {
+        const element = document.getElementById(section);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+
+          return rect.top <= 100 && rect.bottom >= 100;
+        }
+        return false;
+      });
+      setActiveSection(currentSection || "");
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    setActiveSection(pathname.slice(1) || "home");
+  }, [pathname]);
+
+  const navItems = [
+    { name: "Home", href: "#home" },
+    { name: "About Us", href: "#about" },
+    { name: "Contact", href: "#join" },
+  ];
+
   return (
     <>
-
       <div className='pl-2 d-header flex-col items-center '>
         <div>
           <img
-            src="TAHSwami(2).png"
-            alt="The American Hindu Logo"
-            className="mt-1 w-60 h-60 object-cover rounded-full overflow-hidden max-w-[60px] max-h-[60px] bg-red-100" />
+            src='TAHSwami(2).png'
+            alt='The American Hindu Logo'
+            className='mt-1 ml-1 w-60 h-60 object-cover rounded-full overflow-hidden max-w-[60px] max-h-[60px] bg-red-100'
+          />
+          <MdMenu
+            className='open-menu position-absolute top-0 mt-2'
+            size={70}
+            onClick={() => {
+              setMenu(true);
+            }}
+          />
 
+          {menu && (
+            <div className='menu-container menu-enter' id='cool-menu'>
+              <MdClose className='close-menu' onClick={menuLeave} />
+              {/* <p className='menu-btn'>Home</p> */}
+              {navItems.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={`menu-btn pb-10${
+                    activeSection === item.href.slice(1)
+                      ? "menu-btn pb-10 pt-5"
+                      : ""
+                  }`}>
+                  <span className='relative inline-block menu-btn'>
+                    {item.name}
+                  </span>
+                </Link>
+              ))}
+            </div>
+          )}
           {/* <p className='mb-4 ml-6 justify-center font-semibold	mobile-header-the-american'>The American</p>
           <p className =' ml-10 justify-center font-bold		mobile-header-hindu'>Hindu</p> */}
         </div>
-
-
 
         {/* <div className="header-socials">
     <a target="_blank" href="https://google.com"><BsFacebook/></a>
@@ -28,11 +105,9 @@ function MobileHeader() {
     <a target="_blank" href="https://google.com"><BsTwitterX/></a>
     <a target="_blank" href="https://google.com"><BsYoutube/></a>
     </div> */}
-
       </div>
     </>
-  )
+  );
 }
 
 export default MobileHeader;
-
